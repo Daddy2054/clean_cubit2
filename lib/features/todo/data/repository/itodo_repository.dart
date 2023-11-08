52c83057b6804e5b4b9ee28014cbd22bff146e6f
Jean <acalto2@gmail.com>
2023-11-08 20:03:41 +0100

    13.ToDo Repository.mp4

---
+++ lib/features/todo/data/repository/itodo_repository.dart
@@ -0,0 +1,21 @@
+
+
+import '../dto/todo_delete_response.dart';
+import '../dto/todo_response.dart';
+import '../dto/todos_response.dart';
+
+abstract class IToDoRepository {
+
+
+  Future<ToDoResponse> getToDo(int id);
+
+  Future<ToDosResponse> getToDos(int userId);
+
+  Future<ToDosResponse> getToDoList(Map<String,dynamic> queries);
+
+  Future<ToDoResponse> addToDo(Map<String,dynamic> body);
+
+  Future<ToDoResponse> updateToDo(Map<String,dynamic> queries);
+
+  Future<ToDoDeleteResponse> deleteToDo(Map<String,dynamic> queries);
+}
