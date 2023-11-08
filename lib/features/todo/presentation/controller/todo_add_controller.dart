909774581808b45ff2b4a9d39969759574eb4d3f
Jean <acalto2@gmail.com>
2023-11-08 23:31:05 +0100

    18.ToDo Controller.mp4

---
+++ lib/features/todo/presentation/controller/todo_add_controller.dart
@@ -0,0 +1,55 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../core/exception/failure.dart';
+import '../../application/itodo_service.dart';
+import '../state/todo_add_state.dart';
+
+class ToDoAddController extends Cubit<ToDoAddState> {
+  final IToDoService _toDoService;
+
+  ToDoAddController(this._toDoService) : super(const ToDoAddState());
+
+  void addToDo() async {
+    try {
+      emit(state.copyWith(isLoading: true, errorMsg: null));
+      setFormData(key: 'user_id', value: '1');
+
+      if (!state.formData.containsKey('status')) {
+        setFormData(key: 'status', value: '0');
+      }
+
+      final result = await _toDoService.addToDo(state.formData);
+      _setIsAdded(result.id >= 1);
+    } on Failure catch (e) {
+      emit(state.copyWith(
+        isLoading: false,
+        errorMsg: e.message,
+      ));
+    }
+  }
+
+  void setToDoStatus(bool value) {
+    emit(state.copyWith(todoStatus: value));
+    setFormData(key: 'status', value: value ? '1' : '0');
+  }
+
+  void setFormData({required String key, required dynamic value}) {
+    emit(state.copyWith(formData: {
+      ...state.formData,
+      ...{key: value}
+    }));
+  }
+
+  void _setIsAdded(bool value) {
+    emit(state.copyWith(isLoading: false, isAdded: value));
+  }
+
+  void clearState() {
+    emit(state.copyWith(
+      isLoading: false,
+      isAdded: false,
+      todoStatus: false,
+      formData: {},
+    ));
+  }
+}
