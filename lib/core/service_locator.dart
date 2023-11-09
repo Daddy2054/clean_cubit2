6bbb3745c7a10b6d7370eeb6f77f33e9a0f0485e
Jean <acalto2@gmail.com>
2023-11-09 14:32:15 +0100

    19.Inject Dependency with Service Locator.mp4

--- lib/core/service_locator.dart
+++ lib/core/service_locator.dart
@@ -1,5 +1,14 @@
 import 'package:get_it/get_it.dart';
+import 'package:http/http.dart';
 
+import '../features/todo/application/itodo_service.dart';
+import '../features/todo/application/todo_service.dart';
+import '../features/todo/data/api/itodo_api_service.dart';
+import '../features/todo/data/api/todo_api_service.dart';
+import '../features/todo/data/repository/itodo_repository.dart';
+import '../features/todo/data/repository/todo_repository.dart';
+import '../features/todo/presentation/controller/todo_add_controller.dart';
+import '../features/todo/presentation/controller/todo_controller.dart';
 import 'route/go_router_provider.dart';
 
 final getIt = GetIt.instance;
@@ -9,14 +18,14 @@ void serviceLocatorInit() {
 //  getIt.registerFactory(() => GoRouterProvider(getIt.get<RouteObserver>()));
   getIt.registerFactory(() => GoRouterProvider());
 
- // getIt.registerSingleton<Client>(Client());
+ getIt.registerSingleton<Client>(Client());
 
-  // //Feature ToDo
-  // getIt.registerLazySingleton<IToDoApiService>(() => ToDoApiService(getIt<Client>()));
-  // getIt.registerLazySingleton<IToDoRepository>(() => ToDoRepository(getIt<IToDoApiService>()));
-  // getIt.registerLazySingleton<IToDoService>(() => ToDoService(getIt<IToDoRepository>()));
+  //Feature ToDo
+  getIt.registerLazySingleton<IToDoApiService>(() => ToDoApiService(getIt<Client>()));
+  getIt.registerLazySingleton<IToDoRepository>(() => ToDoRepository(getIt<IToDoApiService>()));
+  getIt.registerLazySingleton<IToDoService>(() => ToDoService(getIt<IToDoRepository>()));
 
-  // // ToDO Controller
-  // getIt.registerFactory<ToDoController>(() => ToDoController(getIt<IToDoService>()));
-  // getIt.registerFactory<ToDoAddController>(() => ToDoAddController(getIt<IToDoService>()));
+  // ToDO Controller
+  getIt.registerFactory<ToDoController>(() => ToDoController(getIt<IToDoService>()));
+  getIt.registerFactory<ToDoAddController>(() => ToDoAddController(getIt<IToDoService>()));
 }
