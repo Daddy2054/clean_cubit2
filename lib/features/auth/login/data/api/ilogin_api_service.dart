40bce610bb0fb6477552df000f8698450b5b26d3
Jean <acalto2@gmail.com>
2023-11-16 18:56:27 +0100

    Login API Service.mp4

---
+++ lib/features/auth/login/data/api/ilogin_api_service.dart
@@ -0,0 +1,5 @@
+import 'package:http/http.dart' as http;
+
+abstract class ILoginApiService {
+  Future<http.Response> login(Map<String, dynamic> body);
+}
---
+++ lib/features/auth/login/data/api/login_api_service.dart
@@ -0,0 +1,21 @@
+import 'package:http/http.dart' as http;
+
+import '../../../../../core/mixin/uri_mixin.dart';
+import 'ilogin_api_service.dart';
+
+class LoginApiService with UriMixin implements ILoginApiService {
+  final http.Client client;
+  LoginApiService({
+    required this.client,
+  });
+
+  @override
+  Future<http.Response> login(Map<String, dynamic> body) async {
+    try {
+      final url = getUri('/api/v1/login');
+      return await client.post(url, body: body);
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
